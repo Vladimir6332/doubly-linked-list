6ff904770d70e70821fd2b786365c09f14de99f5
Vladimir Kovalchuk <Vladimir6332@gmail.com>
2019-10-03 17:55:09 +0300

    feat: implement constructor

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,17 +1,43 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._arrNodes = [];
+    }
 
-    append(data) {}
+    append(data) {
+        let newNode = new Node(data);
+        this._arrNodes.push(newNode);
+        if (this.length === 0) {            
+            this._head = newNode;
+            this._tail = newNode;            
+            this.length++;
+        }
+        else {
+            this._tail.next = newNode;
+            newNode.prev = this._tail;            
+            this._tail = newNode;
+            this.length++;
+        }
+    }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        return this._arrNodes[index].data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        if (this._arrNodes[index].data) return
+        this._arrNodes[index].data = data;
+    }
 
     isEmpty() {}
 
